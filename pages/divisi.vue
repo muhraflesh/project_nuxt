7a1821392317f3b8318195523b3cce16e7572229
dewiambar <dewiambarwati318@gmail.com>
2019-07-26 14:32:10 +0700

    divisi2

--- pages/divisi.vue
+++ pages/divisi.vue
@@ -1,20 +1,23 @@
 <template>
-    <section class="section is-desktop main-content columns is-fullheight abu">
-        <div class="container is-desktop">
-            <div class="column is-3 is-narrow-mobile is-fullheight is-hidden-mobile ">
-                <ul class="menu-list">
-                    <a href="#" class="has-background-primary atas has-text-light"> FRONTEND </a>
-                <div class="card-content has-text-left sidenav">
+    <section class="abu">
+        <div class="columns container abu" style="padding-top:40px">
+            <div class="column card is-centered is-3 has-background-light kiri atas">
+                   <header class="card-header has-background-primary">
+                    <p class="card-header-title ">
+                    FRONTEND
+                    </p>
+                </header>
+                    <div class="card-content has-background-light has-text-left">
                         <p><b>Ketua</b><br>Muhammad Rafli S</p><br>
-                        <p><b>Anggota</b></p>
+                         <p><b>Anggota</b></p>
                         <p>Dewi Ambarwati <br> Salma Faiqah A</p>
                     </div>
-                </ul>
             </div>
-           
-           <div class="column" style="padding-left:20%">
-            <div class="row is-4">
-               <div>
+
+               
+            <div class="column is-centered is-9 abu kanan kiri2">
+                <div class="row abu">
+                    <div>
                     <table>
                         <tr><td><b>TASK</b></td></tr>
                         <tr>
@@ -31,7 +34,7 @@
                             Membangun home, halaman login, forgot password, profil
                         </p>
                             </td>
-                            <td style="width:50%">
+                            <td style="width:40%">
                             <span class="icon is-right" style="float:right;">
                                 <i class="fa fa-exclamation-circle merah" aria-hidden="true">
                                 </i>
@@ -41,101 +44,99 @@
                         </div>
                     </div>
                 </div>
-           </div><br>
-           <div class="row is-4">
-               <div class="">
+                </div>
+                <div class="row abu">
+                    <div class="">
                    <td><b>What's Next ?</b></td>
                 <div class="card">
                     <div class="card-content">
                     <td style="width:50%">
                         <span class="icon" style="color:#40e0d0">
-                                <i class="fa fa-clipboard" aria-hidden="true">
+                                <i class="fa fa-edit" aria-hidden="true">
                                 </i>
                         </span>
                         Mengautentikasi halaman yang telah dibuat
                     </td>
-                    <td style="width:5%"><time datetime="2016-1-1">22/07/2019</time></td>
-                    <td style="width:5%"><time datetime="2016-1-1">11:09 PM</time></td>
+                    <td style="width:10%"><time datetime="2016-1-1">22/07/2019</time></td>
+                    <td style="width:10%"><time datetime="2016-1-1">11:09 PM</time></td>
                     </div>
                     <div class="card-content">
                     <td style="width:50%">
                         <span class="icon" style="color:#40e0d0">
-                                <i class="fa fa-clipboard" aria-hidden="true">
+                                <i class="fa fa-edit" aria-hidden="true">
                                 </i>
                         </span>
                         Membuat halaman web responsive</td>
-                    <td style="width:5%"><time datetime="2016-1-1">22/07/2019</time></td>
-                    <td style="width:5%"><time datetime="2016-1-1">11:09 PM</time></td>
+                    <td style="width:10%"><time datetime="2016-1-1">22/07/2019</time></td>
+                    <td style="width:10%"><time datetime="2016-1-1">11:09 PM</time></td>
                     </div>
                     <div class="card-content">
                     <td style="width:50%">
                         <span class="icon" style="color:#40e0d0">
-                                <i class="fa fa-clipboard" aria-hidden="true">
+                                <i class="fa fa-edit" aria-hidden="true">
                                 </i>
                         </span>
                         Menginstall nuxt js</td>
-                    <td style="width:5%"><time datetime="2016-1-1">22/07/2019</time></td>
-                    <td style="width:5%"><time datetime="2016-1-1">11:09 PM</time></td>
+                    <td style="width:10%"><time datetime="2016-1-1">22/07/2019</time></td>
+                    <td style="width:10%"><time datetime="2016-1-1">11:09 PM</time></td>
                     </div>
                     <div class="card-content">
                     <td style="width:50%">
                         <span class="icon" style="color:#40e0d0">
-                                <i class="fa fa-clipboard" aria-hidden="true">
+                                <i class="fa fa-edit" aria-hidden="true">
                                 </i>
                         </span>
                         Menyempurnakan tampilan web</td>
-                    <td style="width:5%"><time datetime="2016-1-1">22/07/2019</time></td>
-                    <td style="width:5%"><time datetime="2016-1-1">11:09 PM</time></td>
+                    <td style="width:10%"><time datetime="2016-1-1">22/07/2019</time></td>
+                    <td style="width:10%"><time datetime="2016-1-1">11:09 PM</time></td>
                     </div>
                     <div class="card-content">
                     <td style="width:50%">
                         <span class="icon" style="color:#40e0d0">
-                                <i class="fa fa-clipboard" aria-hidden="true">
+                                <i class="fa fa-edit" aria-hidden="true">
                                 </i>
                         </span>
                         Menerapkan git secara keseluruhan</td>
-                    <td style="width:5%"><time datetime="2016-1-1">22/07/2019</time></td>
-                    <td style="width:5%"><time datetime="2016-1-1">11:09 PM</time></td>
+                    <td style="width:10%"><time datetime="2016-1-1">22/07/2019</time></td>
+                    <td style="width:10%"><time datetime="2016-1-1">11:09 PM</time></td>
                     </div>
                 </div>
             
                 </div>
-           </div>
-       </div>
-
+                </div>
+            </div>
         </div>
     </section>
 </template>
 
 <style>
-.user{
-    height: 100%;
-    width: 100%
+.kiri{
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    height: 650px;
+    padding-left: 50px;
+
 }
-.sidenav {
-        height: 80%;
-        width: 200px;
-        position: fixed;
-        z-index: 1;
-        top: 0,25;
-        left: 1;
-        background-color: white;
-        overflow-x: hidden;
-        padding-top: 20px;
-    }
-.atas {
-        width: 200px;
+.kanan{
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    height: 650px;
+    width: 100px;
+
 }
-.abu {
+.abu{
     background-color: #e8e8e8;
 }
 .merah {
     color: red;
 }
+.atas {
+        width: 200px;
+}
+.kiri2{
+    padding-left: 50px;
+}
 </style>
 
-<script>
-export default {
-   
-}
-</script>

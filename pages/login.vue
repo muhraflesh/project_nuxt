5d95fbec2c4d17e763651bbcf892bfca24ae756b
muhraflesh <muhraflesh@gmail.com>
2019-09-19 09:50:00 +0700

    raflesh-k38

--- pages/login.vue
+++ pages/login.vue
@@ -33,16 +33,17 @@
                         <input 
                         :type="passwordType" id="password"
                         class="input"
-                        placeholder="password"
+                        placeholder="Password"
                         v-model="password">
                         <span class="icon is-medium is-left">
                           <i class="fa fa-lock"></i>
                         </span>
                       </p>
-                      <p class="control box">
-                        <span><i class="fa fa-eye" :class="[passwordIcon]" @click="hidePassword = !hidePassword" toggle="#password-field"></i></span>
-
-                      </p>
+                      <div class="control box icon is-medium is-left">
+                        <span style="padding-bottom:1%">
+                          <i class="fa fa-eye" :class="[passwordIcon]" @click="hidePassword = !hidePassword" toggle="#password-field"></i>
+                        </span>
+                      </div>
                     </div>
                     <div class="field is-grouped is-grouped-centered">
                       <div class="control">
